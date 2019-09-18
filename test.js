ea18bab8a87e7b2ce30674c85a0bedebc9937bd3
Chang Xiao <xiaochangfeng@gmail.com>
2019-09-18 10:33:59 -0400

    adding script to start tunnel

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
 const request = require('request');
-const fs = require('fs');
 
 // Loading our environment variables
 const BASE_ENV = process.env.TRAKR_BASE_ENV || 'production';
@@ -16,66 +15,64 @@ if (API_TOKEN == undefined || PROJECT_ID == undefined) {
   // Report some type of failure back to the CI
   console.log('Please setup the TRAKR API TOKEN and PROJECT ID in the environment variable settings');
 } else {
-  fs.readFile('/home/circleci/localtunnel.log', 'utf8', function(err, contents) {
-      parts = contents.split(': ');
-      build_url = parts[1].trim();
-      request_body = {
-        'build_url': build_url,
-        'environment': 'production'
+  build_url = 'https://trakr-project-' + PROJECT_ID + 'localtunnel.me';
+  request_body = {
+    'build_url': build_url,
+    'environment': 'production'
+  }
+  options = {
+    url: TRAKR_API + 'project/'+ PROJECT_ID +'/compare_build',
+    headers: {
+      'x-api-key': API_TOKEN,
+    },
+    body: request_body,
+    json: true
+  }
+  // Creating the actual request for testing
+  request.post(options, function(error, response, body) {
+    if (response.statusCode == 200) {
+      // Saving the artifact
+      if (body.status == "success") {
+        fs.writeFile('/home/circleci/trakr_result.log', body.result.url, function(err){
+          console.log('Test URL saved');
+        })
       }
-      options = {
-        url: TRAKR_API + 'project/'+ PROJECT_ID +'/compare_build',
-        headers: {
-          'x-api-key': API_TOKEN,
-        },
-        body: request_body,
-        json: true
-      }
-      request.post(options, function(error, response, body) {
-        if (response.statusCode == 200) {
 
-          // Saving the artifact
-          if (body.status == "success") {
-            fs.writeFile('/home/circleci/trakr_result.log', body.result.url, function(err){
-              console.log('Test URL saved');
-            })
+      // Our anonymous function to check the status of the screenshot
+      x = 0;
+      complete = false;
+      check_status = function(screenshot_id) {
+        // Do the actual API call and get status back
+        options = {
+          url: TRAKR_API + 'status/'+screenshot_id,
+          headers: {
+            'x-api-key': API_TOKEN,
           }
-
-          // Our anonymous function to check the status of the screenshot
-          x = 0;
-          complete = false;
-          check_status = function(screenshot_id) {
-            // Do the actual API call and get status back
-            options = {
-              url: TRAKR_API + 'status/'+screenshot_id,
-              headers: {
-                'x-api-key': API_TOKEN,
-              }
-            }
-            request.get(options, function(error, response, body) {
-              response_body = JSON.parse(body);
-              if (response.statusCode == 200 && response_body.status == 'success') {
-                  progress = response_body.result.progress;
-                  if (response_body.result.message == 'complete') {
-                    console.log('Screenshot ' + screenshot_id + ' is complete');
-                    complete = true;
-                  } else {
-                    console.log('Processing ' + progress + ' for ' + screenshot_id);
-                  }
-              }
-            });
-          }
-
-          // Making sure the test completes (At least the screenshot is created)
-          // Request to Trakr in an interval (1 minute) to check the status
-          intervalID = setInterval(function() {
-            if (complete || ++x == TRAKR_COMPLETION_LIMIT) {
-              clearInterval(intervalID);
-            } else {
-              check_status(body.result.screenshot_created[0]);
-            }
-          }, 1000 * 60);
         }
-      });
+        request.get(options, function(error, response, body) {
+          response_body = JSON.parse(body);
+          if (response.statusCode == 200 && response_body.status == 'success') {
+              progress = response_body.result.progress;
+              if (response_body.result.message == 'complete') {
+                console.log('Screenshot ' + screenshot_id + ' is complete');
+                complete = true;
+              } else {
+                console.log('Processing ' + progress + ' for ' + screenshot_id);
+              }
+          }
+        });
+      }
+
+      // Making sure the test completes (At least the screenshot is created)
+      // Request to Trakr in an interval (1 minute) to check the status
+      intervalID = setInterval(function() {
+        if (complete || ++x == TRAKR_COMPLETION_LIMIT) {
+          clearInterval(intervalID);
+        } else {
+          check_status(body.result.screenshot_created[0]);
+        }
+      }, 1000 * 60);
+    }
   });
+
 }
