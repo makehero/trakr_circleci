ad2e9d50d62a91e7531b548dfce27c6c9a0fa24c
Chang Xiao <xiaochangfeng@gmail.com>
2019-09-23 14:09:45 -0400

    additional script for the orb workflow

--- test.js
+++ test.js
@@ -3,13 +3,15 @@ const fs = require('fs');
 
 // Loading our environment variables
 const BASE_ENV = process.env.TRAKR_BASE_ENV || 'production';
+const DELAY_START = parseInt(process.env.DELAY_START) || 0;
 const API_TOKEN = process.env.TRAKR_API_TOKEN;
 const PROJECT_ID = process.env.TRAKR_PROJECT_ID;
 const BUILD_URL = process.env.BUILD_URL;
+const WORKFLOW_ID = process.env.CIRCLE_WORKFLOW_ID; //provided by CircleCI
 
 // Additional constants
 const TRAKR_API = 'https://app.trakr.tech/api/v1/';
-const TRAKR_COMPLETION_LIMIT = 30; // 30 minute max to wait for screenshot
+const TRAKR_COMPLETION_LIMIT = 60; // 60 minutes max to wait for screenshot
 
 // @todo, we should also consider the working directory (find it from the env)
 
@@ -19,7 +21,8 @@ if (API_TOKEN == undefined || PROJECT_ID == undefined) {
 } else {
   request_body = {
     'build_url': BUILD_URL,
-    'environment': 'production'
+    'environment': 'production',
+    'title': 'CC WF ' + WORKFLOW_ID
   }
   options = {
     url: TRAKR_API + 'project/'+ PROJECT_ID +'/compare_build',
@@ -29,52 +32,57 @@ if (API_TOKEN == undefined || PROJECT_ID == undefined) {
     body: request_body,
     json: true
   }
-  // Creating the actual request for testing
-  // @todo: Wait for the tunnel to start
-  request.post(options, function(error, response, body) {
-    if (response.statusCode == 200) {
-      // Saving the artifact
-      if (body.status == "success") {
-        fs.writeFile('/home/circleci/trakr_result.log', body.result.url, function(err){
-          console.log('Test URL saved');
-        })
-      }
 
-      // Our anonymous function to check the status of the screenshot
-      x = 0;
-      complete = false;
-      check_status = function(screenshot_id) {
-        // Do the actual API call and get status back
-        options = {
-          url: TRAKR_API + 'status/'+screenshot_id,
-          headers: {
-            'x-api-key': API_TOKEN,
-          }
+  // Delaying the screenshot capturing until the tunnel is established if needed
+  setTimeout(function() {
+    // We can always start the test when the build URL becomes available. However
+    // it is probably unreliable just to lookup a http status code
+    console.log('Starting the Trakr visual test');
+    // Creating the actual request for testing
+    request.post(options, function(error, response, body) {
+      if (response.statusCode == 200) {
+        // Saving the artifact
+        if (body.status == "success") {
+          fs.writeFile('/home/circleci/trakr_result.log', body.result.url, function(err){
+            console.log('Test URL saved');
+          })
         }
-        request.get(options, function(error, response, body) {
-          response_body = JSON.parse(body);
-          if (response.statusCode == 200 && response_body.status == 'success') {
-              progress = response_body.result.progress;
-              if (response_body.result.message == 'complete') {
-                console.log('Screenshot ' + screenshot_id + ' is complete');
-                complete = true;
-              } else {
-                console.log('Processing ' + progress + ' for ' + screenshot_id);
-              }
+
+        // Our anonymous function to check the status of the screenshot
+        x = 0;
+        complete = false;
+        check_status = function(screenshot_id) {
+          // Do the actual API call and get status back
+          options = {
+            url: TRAKR_API + 'status/'+screenshot_id,
+            headers: {
+              'x-api-key': API_TOKEN,
+            },
+            json: true
           }
-        });
-      }
-
-      // Making sure the test completes (At least the screenshot is created)
-      // Request to Trakr in an interval (1 minute) to check the status
-      intervalID = setInterval(function() {
-        if (complete || ++x == TRAKR_COMPLETION_LIMIT) {
-          clearInterval(intervalID);
-        } else {
-          check_status(body.result.screenshot_created[0]);
+          request.get(options, function(error, response, body) {
+            if (response.statusCode == 200 && body.status == 'success') {
+                progress = body.result.progress;
+                if (body.result.message == 'complete') {
+                  console.log('Screenshot ' + screenshot_id + ' is complete');
+                  complete = true;
+                } else {
+                  console.log('Processing ' + progress + ' for ' + screenshot_id);
+                }
+            }
+          });
         }
-      }, 1000 * 60);
-    }
-  });
 
+        // Making sure the test completes (At least the screenshot is created)
+        // Request to Trakr in an interval (1 minute) to check the status
+        intervalID = setInterval(function() {
+          if (complete || ++x == TRAKR_COMPLETION_LIMIT) {
+            clearInterval(intervalID);
+          } else {
+            check_status(body.result.screenshot_created[0]);
+          }
+        }, 1000 * 60);
+      }
+    });
+  }, DELAY_START);
 }
---
+++ wait_for_test.js
@@ -0,0 +1,85 @@
+const request = require('request');
+
+const API_TOKEN = process.env.TRAKR_API_TOKEN;
+const PROJECT_ID = process.env.TRAKR_PROJECT_ID;
+const WORKFLOW_ID = process.env.CIRCLE_WORKFLOW_ID; //provided by CircleCI
+
+// Additional constants
+const TRAKR_API = 'https://app.trakr.tech/api/v1/';
+const WAIT_FOR_SN = 6; // try to find the screenshot X times in an every 5 seconds
+const WAIT_FOR_COMPLETE = 60; // 60 minutes max wait time for the tunnel to be kept open
+
+if (API_TOKEN == undefined || PROJECT_ID == undefined) {
+  console.log('Please setup the TRAKR API TOKEN and PROJECT ID in the environment variable settings');
+} else {
+  request_body = {
+    'type': 'screenshot',
+    'title': 'CC WF ' + WORKFLOW_ID
+  }
+  options = {
+    url: TRAKR_API + 'project/'+ PROJECT_ID +'/query',
+    headers: {
+      'x-api-key': API_TOKEN,
+    },
+    body: request_body,
+    json: true
+  }
+  found = false;
+  x = 0;
+
+  // Quick function to find the screenshot started by the test job
+  findSN = function(request_options) {
+    console.log("Waiting for the test to start...")
+    request.post(options, function(error, response, body) {
+      if (body.status == 'success') {
+        screenshot_list = Object.keys(body.result);
+        found = screenshot_list[0]; // we should only get 1 back anyways
+      }
+    });
+  }
+
+  // Function to check for test completion
+  complete = false;
+  y = 0;
+  check_status = function(screenshot_id) {
+    // Do the actual API call and get status back
+    options = {
+      url: TRAKR_API + 'status/'+screenshot_id,
+      headers: {
+        'x-api-key': API_TOKEN,
+      },
+      json: true
+    }
+    request.get(options, function(error, response, body) {
+      if (response.statusCode == 200 && body.status == 'success') {
+          progress = body.result.progress;
+          if (body.result.message == 'complete') {
+            console.log('Screenshot ' + screenshot_id + ' is complete');
+            complete = true;
+          } else {
+            console.log('Processing ' + progress + ' for ' + screenshot_id);
+          }
+      }
+    });
+  }
+
+  // We should first wait to see if the screenshot has been created
+  intervalID = setInterval(function() {
+    if (found || ++x == WAIT_FOR_SN) {
+      clearInterval(intervalID);
+      // if our exit condition is that we found the screenshot collection,
+      // we wait for it to be complete, this is to keep the tunnel open
+      if (found) {
+        CompleteIntervalID = setInterval(function() {
+          if (complete || ++y == WAIT_FOR_COMPLETE) {
+            clearInterval(CompleteIntervalID);
+          } else {
+            check_status(found);
+          }
+        }, 1000 * 60);
+      }
+    } else {
+      findSN(options);
+    }
+  }, 5000);
+}
